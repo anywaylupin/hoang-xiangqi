0121f4504d785f01764e5cb9d99d0757baec16d8
Lupin <lupin.nguyen@outlook.com>
2025-04-18 17:57:37 +0700

    feat: restructure HTML layout and update CSS for improved UI and functionality

--- js/main.js
+++ js/main.js
@@ -1,73 +1,79 @@
 let board = null;
-let game = new Xiangqi();
+const game = new Xiangqi();
+const $status = $('#status');
+const $historyTableBody = $('#history-table tbody');
 
-function removeGreySquares() {
+function removeHighlights() {
   $('#myBoard .square-2b8ce').removeClass('highlight');
 }
 
-function greySquare(square) {
-  let $square = $('#myBoard .square-' + square);
-
-  $square.addClass('highlight');
+function highlightSquare(square) {
+  $('#myBoard .square-' + square).addClass('highlight');
 }
 
 function onDragStart(source, piece) {
-  // do not pick up pieces if the game is over
   if (game.game_over()) return false;
 
-  // or if it's not that side's turn
-  if ((game.turn() === 'r' && piece.search(/^b/) !== -1) || (game.turn() === 'b' && piece.search(/^r/) !== -1)) {
-    return false;
-  }
+  const turn = game.turn();
+  const isWrongTurn = (turn === 'r' && piece.startsWith('b')) || (turn === 'b' && piece.startsWith('r'));
+
+  return !isWrongTurn;
 }
 
 function onDrop(source, target) {
-  removeGreySquares();
+  removeHighlights();
 
-  // see if the move is legal
-  let move = game.move({
-    from: source,
-    to: target
-  });
+  const move = game.move({ from: source, to: target });
+  if (!move) return 'snapback';
 
-  // illegal move
-  if (move === null) return 'snapback';
+  updateStatus();
+  updateHistory();
 }
 
-function onMouseoverSquare(square, piece) {
-  // get list of possible moves for this square
-  let moves = game.moves({
-    square: square,
-    verbose: true
-  });
+function onMouseoverSquare(square) {
+  const moves = game.moves({ square, verbose: true });
+  if (!moves.length) return;
 
-  // exit if there are no moves available for this square
-  if (moves.length === 0) return;
-
-  // highlight the square they moused over
-  greySquare(square);
-
-  // highlight the possible squares for this piece
-  for (let i = 0; i < moves.length; i++) {
-    greySquare(moves[i].to);
-  }
+  highlightSquare(square);
+  moves.forEach(({ to }) => highlightSquare(to));
 }
 
-function onMouseoutSquare(square, piece) {
-  removeGreySquares();
+function onMouseoutSquare() {
+  removeHighlights();
 }
 
 function onSnapEnd() {
   board.position(game.fen());
 }
 
-let config = {
+function updateStatus() {
+  const moveColor = game.turn() === 'r' ? 'Red' : 'Black';
+  $status.text(`${moveColor} turn`);
+}
+
+function updateHistory() {
+  const history = game.history();
+  const rows = [];
+
+  for (let i = 0; i < history.length; i += 2) {
+    const redMove = history[i] || '';
+    const blackMove = history[i + 1] || '';
+    rows.push(`<tr><td>${i / 2 + 1}</td><td>${redMove}</td><td>${blackMove}</td></tr>`);
+  }
+
+  $historyTableBody.html(rows.join(''));
+}
+
+const config = {
   draggable: true,
   position: 'start',
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onMouseoutSquare: onMouseoutSquare,
-  onMouseoverSquare: onMouseoverSquare,
-  onSnapEnd: onSnapEnd
+  onDragStart,
+  onDrop,
+  onMouseoverSquare,
+  onMouseoutSquare,
+  onSnapEnd
 };
+
 board = Xiangqiboard('myBoard', config);
+updateStatus();
+updateHistory();
